162865cf4c81fc226f4e6210b60b1339aa2e1a77
ayuly0 <ayuly@ayuly.ayuly>
2024-09-08 09:39:07 +0700

    style: changing style color of interface

--- QGoodWindow/QGoodWindow/src/darkstyle.cpp
+++ QGoodWindow/QGoodWindow/src/darkstyle.cpp
@@ -26,108 +26,128 @@ SOFTWARE.
 #include "darkstyle.h"
 #include "stylecommon.h"
 
-DarkStyle::DarkStyle() : DarkStyle(styleBase())
+DarkStyle::DarkStyle() : DarkStyle( styleBase() )
 {
 #ifdef Q_OS_WIN
-    m_hash_pixmap_cache[SP_MessageBoxInformation] = StyleCommon::winStandardPixmap(SP_MessageBoxInformation);
-    m_hash_pixmap_cache[SP_MessageBoxWarning] = StyleCommon::winStandardPixmap(SP_MessageBoxWarning);
-    m_hash_pixmap_cache[SP_MessageBoxCritical] = StyleCommon::winStandardPixmap(SP_MessageBoxCritical);
-    m_hash_pixmap_cache[SP_MessageBoxQuestion] = StyleCommon::winStandardPixmap(SP_MessageBoxQuestion);
+    m_hash_pixmap_cache[ SP_MessageBoxInformation ] = StyleCommon::winStandardPixmap( SP_MessageBoxInformation );
+    m_hash_pixmap_cache[ SP_MessageBoxWarning ]     = StyleCommon::winStandardPixmap( SP_MessageBoxWarning );
+    m_hash_pixmap_cache[ SP_MessageBoxCritical ]    = StyleCommon::winStandardPixmap( SP_MessageBoxCritical );
+    m_hash_pixmap_cache[ SP_MessageBoxQuestion ]    = StyleCommon::winStandardPixmap( SP_MessageBoxQuestion );
 #endif
 }
 
-DarkStyle::DarkStyle(QStyle *style) : QProxyStyle(style)
+DarkStyle::DarkStyle( QStyle *style ) : QProxyStyle( style )
 {
-
 }
 
 DarkStyle::~DarkStyle()
 {
-
 }
 
 QStyle *DarkStyle::styleBase() const
 {
-    QStyle *base = QStyleFactory::create(QStringLiteral("Fusion"));
+    QStyle *base = QStyleFactory::create( QStringLiteral( "Fusion" ) );
     return base;
 }
 
-QIcon DarkStyle::standardIcon(StandardPixmap standardPixmap, const QStyleOption *option, const QWidget *widget) const
+QIcon DarkStyle::standardIcon( StandardPixmap standardPixmap, const QStyleOption *option, const QWidget *widget ) const
 {
 #ifdef Q_OS_WIN
-    switch (standardPixmap)
+    switch ( standardPixmap )
     {
     case SP_MessageBoxInformation:
     case SP_MessageBoxWarning:
     case SP_MessageBoxCritical:
-    case SP_MessageBoxQuestion:
-    {
-        QPixmap pixmap = m_hash_pixmap_cache.value(standardPixmap, QPixmap());
+    case SP_MessageBoxQuestion   : {
+        QPixmap pixmap = m_hash_pixmap_cache.value( standardPixmap, QPixmap() );
 
-        if (!pixmap.isNull())
-            return QIcon(pixmap);
+        if ( !pixmap.isNull() )
+        {
+            return QIcon( pixmap );
+        }
 
         break;
     }
-    default:
-        break;
+    default: break;
     }
 #endif
 
-    return QProxyStyle::standardIcon(standardPixmap, option, widget);
+    return QProxyStyle::standardIcon( standardPixmap, option, widget );
 }
 
-void DarkStyle::polish(QPalette &palette)
+void DarkStyle::polish( QPalette &palette )
 {
-    palette.setColor(QPalette::Window, QColor(53, 53, 53));
-    palette.setColor(QPalette::WindowText, QColor(255, 255, 255));
-    palette.setColor(QPalette::Disabled, QPalette::WindowText, QColor(127, 127, 127));
-    palette.setColor(QPalette::Base, QColor(42, 42, 42));
-    palette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
-    palette.setColor(QPalette::ToolTipBase, QColor(255, 255, 255));
-    palette.setColor(QPalette::ToolTipText, QColor(255, 255, 255));
-    palette.setColor(QPalette::Text, QColor(255, 255, 255));
-    palette.setColor(QPalette::Disabled, QPalette::Text, QColor(127, 127, 127));
-    palette.setColor(QPalette::Dark, QColor(35, 35, 35));
-    palette.setColor(QPalette::Shadow, QColor(20, 20, 20));
-    palette.setColor(QPalette::Button, QColor(53, 53, 53));
-    palette.setColor(QPalette::ButtonText, QColor(255, 255, 255));
-    palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(127, 127, 127));
-    palette.setColor(QPalette::BrightText, QColor(255, 0, 0));
-    palette.setColor(QPalette::Link, QColor(42, 130, 218));
-    palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-    palette.setColor(QPalette::Disabled, QPalette::Highlight, QColor(80, 80, 80));
-    palette.setColor(QPalette::HighlightedText, QColor(255, 255, 255));
-    palette.setColor(QPalette::Disabled, QPalette::HighlightedText, QColor(127, 127, 127));
+    QColor windowColor( "#292b2f" );
+    QColor windowTextColor( "#FFFFFF" );
+    QColor disabledTextColor( "#7F7F7F" );
+    QColor baseColor( "#202225" );
+    QColor alternateBaseColor( "#2F3136" );
+    QColor tooltipBaseColor( "#FFFFFF" );
+    QColor tooltipTextColor( "#FFFFFF" );
+    QColor textColor( "#FFFFFF" );
+    QColor darkColor( "#202225" );
+    QColor shadowColor( "#141414" );
+    QColor buttonColor( "#202225" );
+    QColor buttonTextColor( "#FFFFFF" );
+    QColor brightTextColor( "#FF0000" );
+    QColor linkColor( "#2A82DA" );
+    QColor highlightColor( "#2A82DA" );
+    QColor disabledHighlightColor( "#505050" );
+    QColor highlightedTextColor( "#FFFFFF" );
+
+    palette.setColor( QPalette::Window, windowColor );
+    palette.setColor( QPalette::WindowText, windowTextColor );
+    palette.setColor( QPalette::Disabled, QPalette::WindowText, disabledTextColor );
+    palette.setColor( QPalette::Base, baseColor );
+    palette.setColor( QPalette::AlternateBase, alternateBaseColor );
+    palette.setColor( QPalette::ToolTipBase, tooltipBaseColor );
+    palette.setColor( QPalette::ToolTipText, tooltipTextColor );
+    palette.setColor( QPalette::Text, textColor );
+    palette.setColor( QPalette::Disabled, QPalette::Text, disabledTextColor );
+    palette.setColor( QPalette::Dark, darkColor );
+    palette.setColor( QPalette::Shadow, shadowColor );
+    palette.setColor( QPalette::Button, buttonColor );
+    palette.setColor( QPalette::ButtonText, buttonTextColor );
+    palette.setColor( QPalette::Disabled, QPalette::ButtonText, disabledTextColor );
+    palette.setColor( QPalette::BrightText, brightTextColor );
+    palette.setColor( QPalette::Link, linkColor );
+    palette.setColor( QPalette::Highlight, highlightColor );
+    palette.setColor( QPalette::Disabled, QPalette::Highlight, disabledHighlightColor );
+    palette.setColor( QPalette::HighlightedText, highlightedTextColor );
+    palette.setColor( QPalette::Disabled, QPalette::HighlightedText, disabledTextColor );
 }
 
-void DarkStyle::polish(QApplication *app)
+void DarkStyle::polish( QApplication *app )
 {
-    if (!app)
+    if ( !app )
+    {
         return;
+    }
 
     QFont defaultFont = app->font();
-    defaultFont.setPointSize(defaultFont.pointSize() + 2);
-    app->setFont(defaultFont);
+    defaultFont.setPointSize( defaultFont.pointSize() + 2 );
+    app->setFont( defaultFont );
 
-    QFile file(QStringLiteral(":/darkstyle.qss"));
+    QFile file( QStringLiteral( ":/darkstyle.qss" ) );
 
-    if (file.open(QFile::ReadOnly | QFile::Text))
+    if ( file.open( QFile::ReadOnly | QFile::Text ) )
     {
-        QString style_sheet = QLatin1String(file.readAll());
-        app->setStyleSheet(style_sheet);
+        QString style_sheet = QLatin1String( file.readAll() );
+        app->setStyleSheet( style_sheet );
         file.close();
     }
 }
 
-void DarkStyle::unpolish(QApplication *app)
+void DarkStyle::unpolish( QApplication *app )
 {
-    if (!app)
+    if ( !app )
+    {
         return;
+    }
 
     QFont defaultFont = app->font();
-    defaultFont.setPointSize(defaultFont.pointSize() - 2);
-    app->setFont(defaultFont);
+    defaultFont.setPointSize( defaultFont.pointSize() - 2 );
+    app->setFont( defaultFont );
 }
 
 QStyle *DarkStyle::baseStyle() const
